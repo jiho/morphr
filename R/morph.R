e05b29389c0838d731359f04cc8325bbc4091f2a
Jean-Olivier Irisson <irisson@normalesup.org>
2018-03-01 01:15:08 +0100

    R interfaces to python code

---
+++ R/morph.R
@@ -0,0 +1,22 @@
+#' Morph several images into one
+#'
+#' @param paths paths to image files to morph together
+#' @param dest path to a new file in which to write the morphed image. When empty (the default), the morphed image is returned as a matrix. When not empty, the image is written to disk and returned invisibly by the function.
+#' @return A matrix containing the pixel values for the morphed image, in [0,255] (black to white).
+#' @export
+#' @examples
+#' source <- system.file("extdata", "amphipoda", package="morph")
+#' files <- list.files(source, full.names=TRUE)
+#' img <- morph(files)
+#' image(img, col=grey(1:255/255), asp=1)
+#' \dontrun{
+#' img <- morph(files, dest=path.expand("~/amphipoda_morph.jpg"))
+#' }
+morph <- function(paths, dest="") {
+  img <- pymorph$morph(paths, dest)
+  if (dest != "") {
+    invisible(img)
+  } else {
+    img
+  }
+}
---
+++ R/morph_dir.R
@@ -0,0 +1,20 @@
+#' Morph images from a directory into one
+#'
+#' @param path path to the directory containing image files to morph together
+#' @param n how many images to randomly select within the directory. Set this to a very high number to process all
+#' @param write boolean, whether to write the image next to the directiry in which the files are read. When FALSE (the default), the morphed image is returned, as a matrix. When TRUE, the image is written to disk and returned invisibly by the function.
+#' @return A matrix containing the pixel values for the morphed image, in [0,255] (black to white).
+#' @export
+#' @examples
+#' img <- morph_dir(system.file("extdata", "amphipoda", package="morph"))
+#' image(img, col=grey(1:255/255), asp=1)
+#' img <- morph_dir(system.file("extdata", "amphipoda", package="morph"), n=3)
+#' image(img, col=grey(1:255/255), asp=1)
+morph_dir <- function(path, n=15, write=FALSE) {
+  img <- pymorph$morph_dir(path, as.integer(n), write)
+  if (write) {
+    invisible(img)
+  } else {
+    img
+  }
+}
