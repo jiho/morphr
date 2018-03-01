890e9dbcd1b652b23f2c8e5188378d92a2d761a3
Jean-Olivier Irisson <irisson@normalesup.org>
2018-03-01 03:34:36 +0100

    Add a function to display images more cleanly in R

---
+++ R/imshow.R
@@ -0,0 +1,31 @@
+#' Display an array of pixel as a greyscale image
+#'
+#' @param x input array of pixels
+#' @return The (x, y, z) list used to plot the image, which is also suitable for contour, persp, etc. returned invisibly.
+#' @export
+#' @examples
+#' # read an image through python
+#' io <- reticulate::import("skimage.io", as="io")
+#' img <- io$imread(
+#'   system.file("extdata", "amphipoda", "33463695.jpg", package="morphr"),
+#'   as_grey=TRUE
+#' )
+#' # display it
+#' imshow(img)
+imshow <- function(x) {
+  # compute an x, y, z list to force aspect ratio
+  i <- list(
+    x=1:ncol(x),
+    y=1:nrow(x),
+    z=t(x)
+  )
+  image(i,
+    # map intensity to grey levels
+    col=grey(0:254/254),
+    # for 1:1 aspect ratio
+    asp=1,
+    # suppress decorations
+    xaxt="n", yaxt="n", bty="n"
+  )
+  return(invisible(i))
+}
