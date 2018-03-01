6c906e654f8c4c47ae9ac9b013751b559b555bfc
Jean-Olivier Irisson <irisson@normalesup.org>
2018-03-01 03:37:10 +0100

    Update R interfaces to python functions
    - to take into account the new adjust_grey argument
    - to use imshow()

--- R/morph.R
+++ R/morph.R
@@ -2,18 +2,31 @@
 #'
 #' @param paths paths to image files to morph together
 #' @param dest path to a new file in which to write the morphed image. When empty (the default), the morphed image is returned as a matrix. When not empty, the image is written to disk and returned invisibly by the function.
+#' @param adjust_grey whether to adjust the mean grey level of the morphed image to match that of the orignal images it was constructed with. Usually, computing the average(i.e. the morphing) results in lighter images; this compensates it a little. Note that it requires more intense computatin and is therefore switched off by default.
 #' @return A matrix containing the pixel values for the morphed image, in [0,255] (black to white).
 #' @export
 #' @examples
-#' source <- system.file("extdata", "amphipoda", package="morphr")
-#' files <- list.files(source, full.names=TRUE)
-#' img <- morph(files)
-#' image(img, col=grey(1:255/255), asp=1)
+#' amph <- list.files(
+#'           system.file("extdata", "amphipoda", package="morphr"),
+#'           full.names=TRUE
+#'        )
+#' img <- morph(amph)
+#' imshow(img)
+#' img <- morph(amph, adjust_grey=TRUE)
+#' imshow(img)
 #' \dontrun{
 #' img <- morph(files, dest=path.expand("~/amphipoda_morphed.jpg"))
 #' }
-morph <- function(paths, dest="") {
-  img <- pymorph$morph(paths, dest)
+#' cres <- list.files(
+#'           system.file("extdata", "creseidae", package="morphr"),
+#'           full.names=TRUE
+#'        )
+#' img <- morph(cres)
+#' imshow(img)
+#' img <- morph(cres, adjust_grey=TRUE)
+#' imshow(img)
+morph <- function(paths, dest="", adjust_grey=FALSE) {
+  img <- pymorph$morph(paths, dest, adjust_grey)
   if (dest != "") {
     invisible(img)
   } else {
--- R/morph_dir.R
+++ R/morph_dir.R
@@ -3,15 +3,16 @@
 #' @param path path to the directory containing image files to morph together
 #' @param n how many images to randomly select within the directory. Set this to a very high number to process all
 #' @param write boolean, whether to write the image next to the directiry in which the files are read. When FALSE (the default), the morphed image is returned, as a matrix. When TRUE, the image is written to disk and returned invisibly by the function.
+#' @inheritParams morph
 #' @return A matrix containing the pixel values for the morphed image, in [0,255] (black to white).
 #' @export
 #' @examples
-#' img <- morph_dir(system.file("extdata", "amphipoda", package="morphr"))
-#' image(img, col=grey(1:255/255), asp=1)
-#' img <- morph_dir(system.file("extdata", "amphipoda", package="morphr"), n=3)
-#' image(img, col=grey(1:255/255), asp=1)
-morph_dir <- function(path, n=15, write=FALSE) {
-  img <- pymorph$morph_dir(path, as.integer(n), write)
+#' amph <- system.file("extdata", "amphipoda", package="morphr")
+#' imgshow(morph_dir(amph))
+#' imgshow(morph_dir(amph, n=3))
+#' imgshow(morph_dir(amph, n=10, adjust_grey=TRUE))
+morph_dir <- function(path, n=15, write=FALSE, adjust_grey=FALSE) {
+  img <- pymorph$morph_dir(path, as.integer(n), write, adjust_grey)
   if (write) {
     invisible(img)
   } else {
