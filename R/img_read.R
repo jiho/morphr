b636ed6670a6e66dcb5628f678d718e212617e3a
Jean-Olivier Irisson <irisson@normalesup.org>
2021-03-13 21:13:04 +0100

    Allow to invert the image when reading it

--- R/img_read.R
+++ R/img_read.R
@@ -1,6 +1,7 @@
 #' Read a greyscale image
 #'
 #' @param file path to the input image file.
+#' @param invert when TRUE, invert the input image (useful for further processing functions such as [img_moments()] that reaquire light-on-dark input images to make sense.)
 #'
 #' @return A [imager::cimg()] object, which is a four-dimensional numeric array having x,y,z,c coordinates, where x,y are image pixel dimensions, z is depth (for a sequence of images), and c is colour channels (1 for greyscale images, 3 for RGB images, 4 for RGBA images).
 #' @export
@@ -8,9 +9,13 @@
 #' path <- system.file("extdata", "16195419.jpg", package="morphr")
 #' x <- img_read(path)
 #' img_show(x)
-img_read <- function(file) {
+#' x <- img_read(path, invert=TRUE)
+#' img_show(x)
+img_read <- function(file, invert=FALSE) {
   im <- imager::load.image(file)
   # keep only one colour channel
   # TODO support colour images
-  return(im[,,,1,drop=FALSE])
+  im <- im[,,,1,drop=FALSE]
+  if (invert) {im <- 1-im}
+  return(im)
 }
