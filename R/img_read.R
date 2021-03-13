8d9183bc1b911a5837b19fe2ffa3b84ded64cb2c
Jean-Olivier Irisson <irisson@normalesup.org>
2021-03-13 21:14:31 +0100

    Use the pipe in examples

--- R/img_read.R
+++ R/img_read.R
@@ -7,10 +7,10 @@
 #' @export
 #' @examples
 #' path <- system.file("extdata", "16195419.jpg", package="morphr")
-#' x <- img_read(path)
-#' img_show(x)
-#' x <- img_read(path, invert=TRUE)
-#' img_show(x)
+#' img_read(path) %>% img_show()
+#' img_read(path, invert=TRUE) %>% img_show()
+#' # or shorter
+#' img_read(path, i=T) %>% img_show()
 img_read <- function(file, invert=FALSE) {
   im <- imager::load.image(file)
   # keep only one colour channel
