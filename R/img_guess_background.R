4f0d774462989397fc15827f4565b8c238af4ac1
Jean-Olivier Irisson <irisson@normalesup.org>
2021-03-14 10:34:04 +0100

    Add a function to guess the background colour of an image

---
+++ R/img_guess_background.R
@@ -0,0 +1,15 @@
+#' Guess the background colour of an image
+#'
+#' Guess the background colour as the dominant colour at the border of the image.
+#'
+#' @inheritParams img_write
+#'
+#' @export
+#' @examples
+#' x <- img_read(system.file("extdata", "16195419.jpg", package="morphr")) %>% img_show()
+#' img_guess_background(x)
+#' x <- img_read(system.file("extdata", "shape.png", package="morphr")) %>% img_show()
+#' img_guess_background(x)
+img_guess_background <- function(x) {
+  median(c(x[1,], x[,1]))
+}
