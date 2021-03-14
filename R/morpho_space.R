c360fa02c0539621abe82614e5f6da797a495897
Jean-Olivier Irisson <irisson@normalesup.org>
2021-03-14 16:03:04 +0100

    General implementation of a morphospace

--- R/morpho_space.R
+++ R/morpho_space.R
@@ -1,18 +1,23 @@
 #' Build morphological space
 #'
-#' Build a morphological space by computing a Principal Component Analysis on a table of morphological descriptors.
+#' Build a morphological space by performing dimensionality reduction on a table of morphological descriptors.
 #'
 #' @param x data.frame or matrix of morphological descriptors. Should be all numeric and as close to normally-distributed as possible.
-#' @param weights vector of objects weights, with no weights by default. Should have as many elements as `x` has rows but does not need to sum to 1.
-#' @param ... passed to [FactoMineR::PCA()].
+#' @param weights vector of objects weights, with no weights by default. Should have as many elements as rows in `x` but they do not need to sum to 1.
+#' @param method dimensionality reduction method. Currently only "PCA" is implemented (with [FactoMineR::PCA()]).
+#' @param ... passed to the dimensionality reduction function.
 #'
-#' @return An object of class `PCA`, see [FactoMineR::PCA()] for more information.
+#' @return An object of class `morphospace`.
 #'
 #' @export
 #' @examples
-#' s <- morpho_space(plank[,-(1:2)], weights=plank$conc)
+#' s <- morphospace(plank[,-(1:2)], weights=plank$conc)
 #' plot(s, choix="ind")
 #' plot(s, choix="var")
-morpho_space <- function(x, weights=NULL, ...) {
-  FactoMineR::PCA(as.matrix(x), row.w=as.numeric(weights), graph=FALSE, ...)
+morphospace <- function(x, weights=NULL, method="PCA", ...) {
+  if (method == "PCA") {
+    s <- FactoMineR::PCA(as.matrix(x), row.w=as.numeric(weights), graph=FALSE, ...)
+  }
+  class(s) <- c("morphospace", class(s))
+  return(s)
 }
