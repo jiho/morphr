ccc62d50a51f804f808221c4e86029cc21b29eb9
Jean-Olivier Irisson <irisson@normalesup.org>
2021-03-16 12:05:51 +0100

    Add Yeo-Johnson transformation to make data normal

---
+++ R/yeojohnson.R
@@ -0,0 +1,129 @@
+# Most code inspired by https://github.com/petersonR/bestNormalize/blob/master/R/yeojohnson.R
+
+#' Yeo-Johnson transformation
+#'
+#' Perform Yeo-Johnson transformation to attempt normalization.
+#'
+#' @param x a numeric vector of values to make normal.
+#' @param lambda the value of the lambda parameter. When NULL, an appropriate value is estimated from the data.
+#' @param eps the tolerance value under which lamba is considered to be 0 and that is used to choose the appropriate formula.
+#'
+#' @details The Yeo-Johnson is similar to the Box-Cox method, however it allows for the transformation of non-positive data as well.
+#'
+#'@return A list of class \code{yeojohnson} with elements
+#'   \item{data}{original data}
+#'   \item{transformed}{transformed data}
+#'   \item{lambda}{lambda value used for the transformation (possibly estimated)}
+#'
+#' @references Yeo, I. K., & Johnson, R. A. (2000). A new family of power transformations to improve normality or symmetry. Biometrika.
+#'
+#' @seealso [predict.yeojohnson()] to apply the same transformation to a new dataset.
+#'
+#' @export
+#' @examples
+#' # simulate non-normal data
+#' x <- rgamma(100, 1, 1)
+#' hist(x)
+#' xt <- yeojohnson(x)
+#' hist(xt$transformed)
+yeojohnson <- function(x, lambda=NULL, eps=0.001) {
+  stopifnot(is.numeric(x))
+
+  # estimate the value of lambda if needed
+  if (is.null(lambda)) {
+    lambda <- estimate_yeojohnson_lambda(x, eps=eps)
+  }
+
+  # transform non-missing data
+  x_t <- x
+  na_idx <- is.na(x)
+  x_t[!na_idx] <- yeojohnson_trans(x[!na_idx], lambda=lambda, eps=eps)
+
+  # prepare output
+  out <- list(
+    data = x,
+    transformed = x_t,
+    lambda = lambda
+  )
+  class(out) <- c("yeojohnson", class(out))
+  return(out)
+}
+
+#' Transform new data using an already fitted Yeo-Johnson object
+#'
+#' @param object an object of class 'yeojohnson'
+#' @param newdata a numeric vector of data to transform.
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @export
+#' @examples
+#' # simulate non-normal data
+#' x <- rgamma(100, 1, 1)
+#' hist(x)
+#' xt <- yeojohnson(x)
+#' hist(xt$transformed)
+#'
+#' # apply the same transformation to new data
+#' x2 <- rgamma(100, 1, 1)
+#' xt2 <- predict(xt, newdata=x2)
+#' hist(xt2)
+predict.yeojohnson <- function(object, newdata=NULL, ...) {
+  if (is.null(newdata)) {
+    newdata <- object$x
+  }
+
+  # run the transformation
+  na_idx <- is.na(newdata)
+  newdata[!na_idx] <- yeojohnson_trans(newdata[!na_idx], object$lambda)
+
+  return(unname(newdata))
+}
+
+# Helper function that estimates the lambda parameter
+#' @importFrom stats var optimize
+estimate_yeojohnson_lambda <- function(x, lower=-5, upper=5, eps=0.001) {
+  n <- length(x)
+  ccID <- !is.na(x)
+  x <- x[ccID]
+
+  # See Yeo & Johnson Biometrika (2000)
+  yj_loglik <- function(lambda) {
+    x_t <- yeojohnson_trans(x, lambda, eps)
+    x_t_bar <- mean(x_t)
+    x_t_var <- var(x_t) * (n - 1) / n
+    constant <- sum(sign(x) * log(abs(x) + 1))
+    -0.5 * n * log(x_t_var) + (lambda - 1) * constant
+  }
+
+  results <- optimize(yj_loglik, lower=lower,
+                      upper=upper, maximum=TRUE,
+                      tol=.0001)
+
+  return(results$maximum)
+}
+
+# Workhouse function that performs the transformation
+yeojohnson_trans <- function(x, lambda, eps=0.001) {
+  pos_idx <- x >= 0
+  neg_idx <- x < 0
+
+  # Transform positive (non-negative) values
+  if (any(pos_idx)) {
+    if (abs(lambda) < eps) {
+      x[pos_idx] <- log(x[pos_idx] + 1)
+    } else {
+      x[pos_idx] <- ((x[pos_idx] + 1) ^ lambda - 1) / lambda
+    }
+  }
+
+  # Transform negative values
+  if (any(neg_idx)){
+    if (abs(lambda - 2) < eps) {
+      x[neg_idx] <- - log(-x[neg_idx] + 1)
+    } else {
+      x[neg_idx] <- - ((-x[neg_idx] + 1) ^ (2 - lambda) - 1) / (2 - lambda)
+    }
+  }
+
+  return(x)
+}
